5ee209db3b455c69757ddb43b35e421c508a11db
kenshunn <aaron.11demartin@gmail.com>
2024-02-11 01:13:20 +0800

    AUTH & Token, na pinagpuyatan

--- TwitterCloneUi/loginstyle.js
+++ TwitterCloneUi/loginstyle.js
@@ -1,73 +1,3 @@
-function regisPass() {
-    const username = document.getElementById('username-regis').value;
-    const password = document.getElementById('password-regis').value;
-    const confirmPass = document.getElementById('confirm-password').value;
-
-    if (password !== confirmPass) {
-        alert("Password do not match. Please re-enter your password.");
-        return false;
-    } else {
-        register(username, password);
-    }
-}
-
-async function register(username, password) {
-    const regisData = {
-      username,
-      password,
-    };
-  
-    const response = await fetch('http://localhost:3000/api/v1/auth/register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(regisData),
-    });
-  
-    if (!response.ok) {
-      throw new Error('Registration failed: ' + (await response.text()));
-    }
-  
-}
-
-
-async function logIn() {
-    const username = document.getElementById('username-log').value;
-    const password = document.getElementById('password-log').value;
-    sendDatatoLS(username, password);
-}
-
-
-async function sendDatatoLS(username, password) {
-    const res = await fetch("http://localhost:3000/api/v1/auth/login", {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            'username': username,
-            'password': password
-        })
-    })
-    if (res.ok) {
-        const token = await res.text();
-        localStorage.setItem('token', token);
-    } else {
-        const error = await res.json();
-        console.error(error);
-    }
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-    const loginButton = document.getElementById("login-button");
-
-    loginButton.addEventListener("click", async function (event) {
-        event.preventDefault(); // Prevent the default form submission
-        await logIn(); // Call the login function when the button is clicked
-    });
-});
-
 function logSign(){
     let x = document.getElementById('login-box');
     let y = document.getElementById('signup-box');
@@ -81,7 +11,6 @@ function logSign(){
     }
 }
 
-<<<<<<< HEAD
 function regisPass() {
     const username = document.getElementById('username-regis').value;
     const password = document.getElementById('password-regis').value;
@@ -160,8 +89,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-=======
->>>>>>> 4de5a36105ccb42f41572f4432dd6eea3f8f5833
 var signx = document.getElementById('signup-btn');
 var signy = document.getElementById('login-btn');
 
