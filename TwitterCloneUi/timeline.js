927972dc53933e8b9aa67833d4b28e35d79919cf
IanSalandanan <kerbysalandanan@gmail.com>
2024-02-10 19:13:21 +0800

    minor fix

--- TwitterCloneUi/timeline.js
+++ TwitterCloneUi/timeline.js
@@ -1,2 +1,21 @@
-import PostRepository from "../TwitterCloneApi/repositories/PostRepository";
+document.getElementsByClassName("post-button").addEventListener("click", event => {
+    event.preventDefault();
+
+    const textAreaValue = document.getElementsByClassName("text-area").value;
+
+    fetch('http://localhost:3000/api/v1/posts', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ text: textAreaValue})
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("API response: ", data)
+    })
+    .catch(error => {
+        console.error("API error: ", error);
+    });
+})
 
